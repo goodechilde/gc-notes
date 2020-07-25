291f8a02fb9f510a2c60bf80c898bea0f0a28062
Sean Shoffstall <sean+shelby@sycle.net>
2020-07-25 08:51:08 -0700

    :bug: Changed note length from string to text

--- src/database/migrations/2020_07_24_213318_create_note_types_table.php
+++ src/database/migrations/2020_07_24_213318_create_note_types_table.php
@@ -15,7 +15,7 @@ class CreateNoteTypesTable extends Migration
     {
         Schema::create('note_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->text('name');
             $table->timestampsTz();
             $table->softDeletesTz();
         });
